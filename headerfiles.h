edf2986059052164fed25166e713c0a3fdc2dc68
quanrocks11 <Qnguyen0001@gmail.com>
2021-11-01 17:07:47 -0500

    adding headerfiles and test

---
+++ headerfiles.h
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<fstream>
+#include<math.h>
+#include<stdlib.h>
+#include<time.h>
+#include<windows.h>
+#include<ctype.h>
+#include<string>
+#include<string>
+#include<conio.h>
+#include<vector>
+#include<iomanip>
+
+using namespace std;
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include "headerfiles.h"
+
+int main (void)
+{
+	cout<<"Hello world\n";
+	
+	system("Pause");
+	return 0;
+}
